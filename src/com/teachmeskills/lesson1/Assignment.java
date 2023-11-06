21bc35517fd601eeb0ced9b90dc65f9dc770bcd2
MarkSlepter <131391955+MarkSlepter@users.noreply.github.com>
2023-11-06 15:41:27 +0100

    First commit

---
+++ src/com/teachmeskills/lesson1/Assignment.java
@@ -0,0 +1,7 @@
+package com.teachmeskills.lesson1;
+
+public class Assignment {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/teachmeskills/lesson1/Assignment2.java
@@ -0,0 +1,12 @@
+package com.teachmeskills.lesson1;
+
+import java.util.Scanner;
+
+class Assignment2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String user_name = scanner.nextLine();
+        System.out.println("Hello, "+user_name+ "!");
+    }
+
+}
